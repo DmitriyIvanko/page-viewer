a95237494c435ac38c8409c2a3572d9fb4b67e57
DmitriyIvanko <ivanko.dmitriy@gmail.com>
2026-03-25 10:00:38 +0300

    + add page viewer;

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PageViewer } from './page-viewer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PageViewer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/page-viewer/page-viewer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-viewer',
+  imports: [],
+  templateUrl: './page-viewer.html',
+  styleUrl: './page-viewer.scss',
+})
+export class PageViewer {
+
+}
