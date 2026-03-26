f73741f1bd27e53327e2615176941b9cc9dd7008
DmitriyIvanko <ivanko.dmitriy@gmail.com>
2026-03-26 09:41:14 +0300

    + add scroll positions;

--- src/app/page-viewer/page-viewer.const.ts
+++ src/app/page-viewer/page-viewer.const.ts
@@ -1,5 +1,7 @@
+export const PRELOAD_PAGES_COUNT = 1;
+export const INITIAL_ZOOM = 1;
+
 // assume this is page view/height the same for every page and can not be changed by user:
 export const PAGE_WIDTH_PX = 794;
 export const PAGE_HEIGHT_PX = 1_123;
 
-export const INITIAL_ZOOM = 1;
--- src/app/page-viewer/page-viewer.ts
+++ src/app/page-viewer/page-viewer.ts
@@ -1,12 +1,12 @@
-import { NgStyle } from '@angular/common';
+import { JsonPipe, NgStyle } from '@angular/common';
 import { Component, computed, signal } from '@angular/core';
 
 import { MOCK_DATA } from './api.mock';
-import { INITIAL_ZOOM, PAGE_HEIGHT_PX, PAGE_WIDTH_PX } from './page-viewer.const';
+import { INITIAL_ZOOM, PAGE_HEIGHT_PX, PAGE_WIDTH_PX, PRELOAD_PAGES_COUNT } from './page-viewer.const';
 
 @Component({
   selector: 'app-page-viewer',
-  imports: [NgStyle],
+  imports: [NgStyle, JsonPipe],
   templateUrl: './page-viewer.html',
   styleUrl: './page-viewer.scss',
 })
@@ -16,7 +16,36 @@ export class PageViewer {
   readonly pageList = computed(() => this.data().pages);
   readonly pageWidth = signal(PAGE_WIDTH_PX)
   readonly pageHeight = signal(PAGE_HEIGHT_PX);
-  readonly totalHeight = computed(() => this.pageWidth() * this.pageList().length * this.zoom())
+  readonly totalHeight = computed(() => this.pageWidth() * this.pageList().length * this.zoom());
   readonly zoom = signal(INITIAL_ZOOM);
+  readonly renderedPages = computed(() => {
+    const totalPage = this.pageList().length;
+    const currentPage = Math.trunc(totalPage * this.scrollRatio());
+    console.log(currentPage);
+    return {
+      start: Math.max(0, currentPage - PRELOAD_PAGES_COUNT),
+      end: Math.min(totalPage, currentPage + PRELOAD_PAGES_COUNT),
+    };
+  });
+  private readonly scrollRatio = signal<number>(0);
+
+  onVirtualScroll(event: Event): void {
+    const scrollContainer = event.currentTarget as HTMLDivElement | null;
+
+    if (scrollContainer == null) { // lint smart for ==
+      return;
+    }
+
+    const scrollHeight = scrollContainer.scrollHeight;
+    const scrollTop = scrollContainer.scrollTop;
+    const clientHeight =  scrollContainer.clientHeight;
+
+    const scrollRatio = scrollHeight === 0
+      ? 0
+      : (scrollTop + clientHeight) / scrollHeight;
+
+    // console.log(scrollRatio)
+    this.scrollRatio.set(scrollRatio);
+  }
 
 }
