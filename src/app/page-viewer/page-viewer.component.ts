b27cdc5d2d0969d57a568ddf7e3bac674f450c9b
DmitriyIvanko <ivanko.dmitriy@gmail.com>
2026-03-31 10:02:55 +0300

    - remove not used;

--- src/app/page-viewer/page-viewer.component.ts
+++ src/app/page-viewer/page-viewer.component.ts
@@ -1,10 +1,11 @@
-import { ChangeDetectionStrategy, Component, computed, DestroyRef, effect, inject, Injector, OnInit, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, DestroyRef, effect, ElementRef, inject, Injector, OnInit, signal, viewChild } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
 
 import { DOCUMENT_ID_PATH } from '../app.const';
 import { DocumentStateService } from './core';
 import { INITIAL_ZOOM, MAX_ZOOM, MIN_ZOOM, PAGE_HEIGHT_PX, PAGE_WIDTH_PX, PRELOAD_PAGES_COUNT, ZOOM_STEP } from './page-viewer.const';
+import { ScrollHelper } from './scroll-helper';
 
 @Component({
   selector: 'pv-page-viewer',
@@ -24,7 +25,7 @@ export class PageViewerComponent implements OnInit {
 
     return Math.min(totalPage - 1, Math.trunc(totalPage * this.scrollRatioS()));
   })
-  readonly isLoadingS = computed(() => this.documentStateService.isLoadingS || false); // TODO: add other service
+  readonly isLoadingS = computed(() => this.documentStateService.isLoadingS() || false); // TODO: add other service
   readonly pageHeightS = computed(() => this.zoomS() * PAGE_HEIGHT_PX);
   readonly pageListS = computed(() => this.documentStateService.stateS()?.pageList ?? []);
   readonly pageWidthS = computed(() => this.zoomS() * PAGE_WIDTH_PX);
@@ -48,16 +49,20 @@ export class PageViewerComponent implements OnInit {
   private readonly destroyRef = inject(DestroyRef);
   private readonly documentIdS = signal<string | null>(null);
   private readonly documentStateService = inject(DocumentStateService);
+  private readonly renderedPagesElementS = viewChild<ElementRef<HTMLDivElement>>('renderedPagesElement');
+  private readonly scrollHelper = inject(ScrollHelper);
   private readonly scrollRatioS = signal<number>(0);
   private readonly injector = inject(Injector);
 
   ngOnInit(): void {
+    // get id:
     this.activatedRoute.paramMap.pipe(takeUntilDestroyed(this.destroyRef)).subscribe((paramMap) => {
       const documentId = paramMap.get(DOCUMENT_ID_PATH);
 
       this.documentIdS.set(documentId);
     });
 
+    // load data:
     effect(() => {
       const documentId = this.documentIdS();
 
@@ -66,6 +71,44 @@ export class PageViewerComponent implements OnInit {
       }
 
       this.documentStateService.get(documentId);
+    }, { injector: this.injector });
+
+    // virtual scroll:
+    effect(() => {
+      const renderedPagesElement = this.renderedPagesElementS()?.nativeElement as HTMLDivElement | undefined;
+      const currentPageIndex = this.currentVisiblePageIndexS();
+      const totalHeightValue = this.totalHeightS();
+      const renderedPagesCount = this.renderedPageListS().length;
+      const totalPageCount = this.pageListS().length;
+      const renderedTotalHeight = renderedPagesCount * this.pageHeightS();
+
+      if (renderedPagesElement == null || totalHeightValue === 0 || renderedTotalHeight === 0) {
+        return;
+      }
+
+      const scrollToRatio = this.scrollHelper.calcPageScrollRatio({
+        clientHeight: renderedPagesElement.clientHeight,
+        currentPageIndex,
+        renderedPagesCount,
+        renderedTotalHeight,
+        scrollRatio: this.scrollRatioS(),
+        totalHeightValue,
+        totalPageCount,
+        zoom: this.zoomS(),
+      })
+
+      if (scrollToRatio == null) {
+        console.warn('Data inconsistent');
+        return;
+      }
+
+      const scrollTo = scrollToRatio * renderedTotalHeight;
+
+      requestAnimationFrame(() => {
+        renderedPagesElement.scroll({
+          top: scrollTo,
+        });
+      });
     }, { injector: this.injector });
   }
 
